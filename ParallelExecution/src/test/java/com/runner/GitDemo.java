f740b0201a735c83d1352dad7c6f22dd4664c762
deepasirsat <61134456+deepasirsat@users.noreply.github.com>
2021-01-13 15:46:28 +0530

    Update GitDemo.java
    added changes on gitDeno

--- ParallelExecution/src/test/java/com/runner/GitDemo.java
+++ ParallelExecution/src/test/java/com/runner/GitDemo.java
@@ -3,6 +3,7 @@ package com.runner;
 public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello Git Demo");
+		System.out.println("Hello  Pull Git Demo");
 	}
 
 }
